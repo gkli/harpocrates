fce86651b72020063042514e6a9a5dd32db7c13d
Gary Klionsky <gary@klionsky.com>
2020-09-23 22:30:25 -0700

    js changes

--- Src/Harpocrates.Management.Web/wwwroot/js/common/footer.js
+++ Src/Harpocrates.Management.Web/wwwroot/js/common/footer.js
@@ -4,12 +4,24 @@ window.Harpocrates.ui = window.Harpocrates.ui || {};
 window.Harpocrates.ui.footer = (function ($, enums, common, undefined) {
     if (!common) throw "common is required";
 
-    common.templates.load().done(function () {
+    function _createViewModel() {
         var vm = {
             year: new Date().getFullYear()
         };
 
-        ko.applyBindings(vm, $('div.template-footer')[0]);
-    });
+        return vm;
+    }
+
+    //common.templates.load().done(function () {
+    //    var vm = {
+    //        year: new Date().getFullYear()
+    //    };
+
+    //    ko.applyBindings(vm, $('div.template-footer')[0]);
+    //});
+
+    return {
+        createFooterViewModel: _createViewModel
+    };
 
 })(window.jQuery, window.Harpocrates.enums, window.Harpocrates.common);
---
+++ Src/Harpocrates.Management.Web/wwwroot/js/public/app.js
@@ -0,0 +1,33 @@
+﻿window.Harpocrates = window.Harpocrates || {};
+
+window.Harpocrates.app = (function ($, data, enums, common, security, undefined) {
+
+    function _createViewModel() {
+
+        function featuredItem(item, template) {
+            var self = this;
+
+            self.template = ko.observable(template);
+            self.item = ko.observable(item);
+        }
+
+        var navVm = window.Harpocrates.ui.menu.createNavViewModel();
+
+        var vm = {
+            nav: {
+                menu: navVm.menu,
+                sidebar: navVm.sidebar,
+                footer: window.Harpocrates.ui.footer.createFooterViewModel()
+            },
+            body: {
+                pages: new data.common.entities.collection()
+            }
+        };
+
+        return vm;
+    }
+
+    return {
+        createViewModel: _createViewModel
+    };
+})(window.jQuery, window.Harpocrates.viewModels, window.Harpocrates.enums, window.Harpocrates.common, window.Harpocrates.security);
